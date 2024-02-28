63689ca2ce674965377f9aef36ca2f8a1548e8b0
@Arustat <mayssou839@gmail.com>
2024-02-28 15:21:57 +0100

    .

--- pages/page_accueil.php
+++ pages/page_accueil.php
@@ -83,10 +83,6 @@
         <div class="presentation">
             <p class="presentation_ecriture"> <strong>Y'Music, qu'est-ce que c'est ?</strong> Y'Music est une plateforme de rencontre permettant aux utilisateurs de participer à des concerts hors du commun tout en faisant de belles rencontres ! </p>
         </div>
-
-
-
-
                     <div class="card">
                         <div class="content-card">
                             <div class="festi_pic1">
